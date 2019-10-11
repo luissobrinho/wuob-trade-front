e04f21e9bc569272e88a8da1eca187f6c42ed65d
Igor Marti <igorm573@gmail.com>
2019-10-11 20:56:33 -0300

    Implemented change pass

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  urlApi: 'http://127.0.0.1:8000/api/v1/',
+  firebaseConfig: {
+    apiKey: "AIzaSyC-gsIybRxDiDOmUGFJheHDEOSgHG-6qs4",
+    authDomain: "trade-dbbf3.firebaseapp.com",
+    databaseURL: "https://trade-dbbf3.firebaseio.com",
+    projectId: "trade-dbbf3",
+    storageBucket: "",
+    messagingSenderId: "615076606227",
+    appId: "1:615076606227:web:b2dd9ae3a71f8149e3eae6"
+  },
+  urlAngular: 'http://localhost:4200/authentication/signup'
 };
 
 /*
