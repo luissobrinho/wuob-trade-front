41b88217e6c53dfeb54526ffd587385babc2aeb6
Cesar Felipe <metaces@gmail.com>
2019-10-11 21:37:30 -0300

    criacao de wallet

--- src/app/loot/wallet/wallet.component.ts
+++ src/app/loot/wallet/wallet.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { LootService } from 'src/app/services/loot/loot.service';
+import { Events } from '@ionic/angular';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Wallet } from 'src/app/models/Wallet';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-wallet',
@@ -7,9 +15,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalletComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('modal', { static: true }) modal;
+
+  submitted = false;
+  walletForm: FormGroup;
+  nome: string;
+  hash: string;
+
+  constructor(public loot: LootService, public events: Events, private ngxService: NgxUiLoaderService,
+    private formBuilder: FormBuilder, public router: Router, private modalService: NgbModal) { }
 
   ngOnInit() {
+    this.initForm();
+    this.initValues();
+  }
+
+  initForm() {
+    this.walletForm = this.formBuilder.group({
+      nome: ['', Validators.compose([Validators.required])],
+      hash: ['', Validators.compose([Validators.required])],
+    });
+  }
+
+  initValues() {
+    let user = JSON.parse(sessionStorage.getItem('currentUser'));
+  }
+
+  get f() { return this.walletForm.controls; }
+
+  createWallet(){
+    this.submitted = true
+
+    if (this.walletForm.invalid) {
+      return;
+    }
+    this.ngxService.start();
+
+    this.loot.createWallet(this.walletForm.value).then((response: Wallet) => {
+      console.log(response);
+
+      this.ngxService.stop();
+      this.nome = response.nome;
+      this.hash = response.hash;
+      this.events.publish('toast', 'Success', 10000, 'toast-success')
+      // let modalRef = this.openModal();
+      // modalRef['valor'] = response.valor;
+      // modalRef['status_text'] = response.status_text;
+    }).catch((err: HttpErrorResponse) => {
+
+      this.ngxService.stop();
+      this.events.publish('toast', err, 'Erro', 10000, 'toast-error')
+    }), err => {
+      this.ngxService.stop();
+      this.events.publish('toast', err, 'Erro', 10000, 'toast-error')
+    }
+  }
+
+  openModal() {
+    return this.modalService.open(this.modal, { centered: true });
   }
 
 }
