3efedc1b360a8cf27c531af765e8ab36b02ecd21
cesar felipe reis alberto <metaces@gmail.com>
2020-01-31 20:00:06 -0300

    renomeado arquivos e ajustado para wuobgames link de referencia

--- src/app/services/translation/translation.service.ts
+++ src/app/services/translation/translation.service.ts
@@ -8,11 +8,13 @@ export class TranslationService {
 
   constructor(public translate:TranslateService) {}
 
-  configLang(langDefault:string = 'en'){
-    this.translate.addLangs(['en','pt','fr'])
+  configLang(langDefault:string = 'en-US'){
+    this.translate.addLangs(['en-US','pt-BR','fr-FR','pt-PT'])
     this.translate.setDefaultLang(langDefault)
-    const browserlang = this.translate.getBrowserLang()
-    this.translate.use(browserlang.match(/en|fr/)? browserlang:langDefault)  
+    const browserlang = this.translate.getBrowserLang();
+    const browserCultureLang = this.translate.getBrowserCultureLang();
+    const checkedBrowserCultureLang = browserCultureLang.match(/pt-BR/)? 'pt-PT' : browserCultureLang;
+    this.translate.use(checkedBrowserCultureLang.match(/en-US|fr-FR|pt-PT/)? browserCultureLang:langDefault)  
   }
 
 }
